c96006ff38a9595d8028c7ffcf9e81b921341d1f
Gauri Behl <140520216+gauribehl100@users.noreply.github.com>
2025-03-17 03:24:04 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
    server: {
     host: '0.0.0.0',
-    port: 10000       
+    port: 10000,
+    allowedHosts: ['frontend-coderev-2.onrender.com'],
   }
 })
