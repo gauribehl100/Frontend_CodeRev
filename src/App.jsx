82ded0e6c1870b73efa0eac10ee829d36fb36009
Gauri Behl <140520216+gauribehl100@users.noreply.github.com>
2025-03-17 13:35:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   setAudioUrl(null); 
 
     try {
-      const response = await axios.post("https://backend-code-1-vriw.onrender.com", {
+      const response = await axios.post("https://backend-code-2-xgru.onrender.com", {
         prompt: code,
         language: language,
       });
