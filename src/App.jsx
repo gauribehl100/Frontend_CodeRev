cd5a22a6e5230d87d24be74034f4c46c739fd007
Gauri Behl <140520216+gauribehl100@users.noreply.github.com>
2025-03-17 14:49:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
   setAudioUrl(null); 
 
     try {
-      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL ||"https://backend-code-3-1qmp.onrender.com"}/ai/response`, {
+      const response = await axios.post(`${import.meta.env.VITE_BACKEND_URL ||"https://backend-code-4-azlk.onrender.com"}/ai/response`, {
         prompt: code,
         language: language,
       });
